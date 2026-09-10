4812b9b47e7117705fca646a5d5979a1c9a37038
mdrazzakhosen009-cpu <mdrazzakhosen009@gmail.com>
2026-09-11 04:58:25 +0600

    Delete scripts directory

--- scripts/init-db.js
+++
@@ -1,3 +0,0 @@
-require('dotenv').config();
-const {initDb,verifyConnection}=require('../src/db');
-verifyConnection().then(()=>initDb()).then(()=>{console.log('Database initialized');process.exit(0);}).catch(e=>{console.error(e);process.exit(1);});
